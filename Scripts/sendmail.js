e165d4e540dffcff085944394a04f9233cd922a6
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-08 13:20:56 +0100

    pre-loader

--- Scripts/sendmail.js
+++ Scripts/sendmail.js
@@ -1,13 +1,39 @@
-function sendEmail(event){
+function sendEmail(event) {
     event.preventDefault();
-    var params={
-        from_name:document.getElementById('name').value,
-        from_email:document.getElementById('Email').value,
-        from_subject:document.getElementById('subject').value,
-        message:document.getElementById('message').value
+    
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('Email');
+    var subjectInput = document.getElementById('subject');
+    var messageInput = document.getElementById('message');
+  
+    // Check if all required fields are filled
+    if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || subjectInput.value.trim() === '' || messageInput.value.trim() === '') {
+      alert('Please fill in all required fields.');
+      return;
     }
-    emailjs.send("service_ttcd701","template_xxu4ucw",params).then(function(res){
-        alert("Email Sent Successfully!");
-        location.reload();
-})
-}
+  
+    var loader = document.getElementById('preloader');
+    var btn = document.getElementById('btnid');
+    var params = {
+      from_name: nameInput.value,
+      from_email: emailInput.value,
+      from_subject: subjectInput.value,
+      message: messageInput.value
+    };
+  
+    loader.style.display = 'block';
+    btn.style.display = 'none';
+  
+    emailjs.send('service_ttcd701', 'template_xxu4ucw', params).then(function(res) {
+      loader.style.display = 'none';
+      btn.style.display = 'block';
+      alert('Email Sent Successfully!');
+      location.reload();
+    });
+  }
+  
+  window.addEventListener('load', function() {
+    var loader = document.getElementById('preloader');
+    loader.style.display = 'none';
+  });
+  
