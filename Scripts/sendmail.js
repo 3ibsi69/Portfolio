1d7a81c0284148844f46cc422095ec3c82cdecdc
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-08 11:38:53 +0100

    Email-work

---
+++ Scripts/sendmail.js
@@ -0,0 +1,26 @@
+function sendEmail(event) {
+    event.preventDefault();
+    
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('Email');
+    var subjectInput = document.getElementById('subject');
+    var messageInput = document.getElementById('message');
+    
+    var params = {
+      from_name: nameInput.value,
+      from_email: emailInput.value,
+      from_subject: subjectInput.value,
+      message: messageInput.value
+    };
+    
+    emailjs.send("service_ttcd701", "template_xxu4ucw", params)
+      .then(function(res) {
+        alert("Sent Successfully: " + res.status);
+        location.reload();
+      })
+      .catch(function(error) {
+        console.error('Error sending email:', error);
+        alert("An error occurred while sending the email. Please try again later.");
+      });
+  }
+  
