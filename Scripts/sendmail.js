cf5bd8bae66be8019d383b64a6847c94583f6146
3ibsi69 <a.abbassi21306@pi.tn>
2023-06-08 11:42:45 +0100

    js

--- Scripts/sendmail.js
+++ Scripts/sendmail.js
@@ -1,26 +1,13 @@
-function sendEmail(event) {
+function sendEmail(event){
     event.preventDefault();
-    
-    var nameInput = document.getElementById('name');
-    var emailInput = document.getElementById('Email');
-    var subjectInput = document.getElementById('subject');
-    var messageInput = document.getElementById('message');
-    
-    var params = {
-      from_name: nameInput.value,
-      from_email: emailInput.value,
-      from_subject: subjectInput.value,
-      message: messageInput.value
-    };
-    
-    emailjs.send("service_ttcd701", "template_xxu4ucw", params)
-      .then(function(res) {
-        alert("Email Sent Successfully!");
+    var params={
+        from_name:document.getElementById('name').value,
+        from_email:document.getElementById('Email').value,
+        from_subject:document.getElementById('subject').value,
+        message:document.getElementById('message').value
+    }
+    emailjs.send("service_ttcd701","template_xxu4ucw",params).then(function(res){
+        alert("Sent Successfully");
         location.reload();
-      })
-      .catch(function(error) {
-        console.error('Error sending email:', error);
-        alert("An error occurred while sending the email. Please try again later.");
-      });
-  }
-  
+})
+}
